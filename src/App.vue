b25761bd90c9fd9c15d5d28da443a0f871f753f0
Douglas Amaral <douglaxz@hotmail.com>
2023-05-04 11:28:17 -0300

    segundo commit

--- src/App.vue
+++ src/App.vue
@@ -36,11 +36,11 @@ export default{
 
   </main>
   <footer>
-    <a href="http://www.facebook.com/douglas.amaral2306" target="_blank"><i class="bi bi-facebook" style="font-size: 3rem; color: white;"></i></a>
-    <a href="http://www.github.com/Douglaxz" target="_blank"><i class="bi bi-github" style="font-size: 3rem; color: white;"></i></a>
-    <a href="http://www.linkedin.com/douglas.amaral-dga" target="_blank"><i class="bi bi-linkedin" style="font-size: 3rem; color: white;"></i></a>
-    <a href="http://www.instagram.com/douglaxz" target="_blank"><i class="bi bi-instagram" style="font-size: 3rem; color: white;"></i></a>
-    <a href="http://www.webwhatsapp.com/5541996686000" target="_blank"><i class="bi bi-whatsapp" style="font-size: 3rem; color: white;"></i></a>
+    <a href="http://www.facebook.com/douglas.amaral2306" target="_blank"><i class="bi bi-facebook" style="font-size: 2rem; color: white;"></i></a>
+    <a href="http://www.github.com/Douglaxz" target="_blank"><i class="bi bi-github" style="font-size: 2rem; color: white;"></i></a>
+    <a href="https://www.linkedin.com/in/douglas-amaral-dga/" target="_blank"><i class="bi bi-linkedin" style="font-size: 2rem; color: white;"></i></a>
+    <a href="http://www.instagram.com/douglaxz" target="_blank"><i class="bi bi-instagram" style="font-size: 2rem; color: white;"></i></a>
+    <a href="http://www.webwhatsapp.com/5541996686000" target="_blank"><i class="bi bi-whatsapp" style="font-size: 2rem; color: white;"></i></a>
   </footer>
 </template>
 
--- src/views/hobbie.vue
+++ src/views/hobbie.vue
@@ -9,25 +9,27 @@
 </script>
 <template>
     <h1>Hobbies:</h1>
-    <table>
-        <tr>
-            <td><img src="https://img.shields.io/badge/Playstation-003791?style=for-the-badge&logo=playstation&logoColor=white" height="50" width="300"></td>
-            <td><img src="https://img.shields.io/badge/Playstation%202-003791?style=for-the-badge&logo=playstation-2&logoColor=white" height="50" width="300"></td>
-            <td><img src="https://img.shields.io/badge/Playstation%203-003791?style=for-the-badge&logo=playstation-3&logoColor=white" height="50" width="300"></td>
-        </tr>
-
-        <tr>
-            <td><img src="https://img.shields.io/badge/Playstation%204-003791?style=for-the-badge&logo=playstation-4&logoColor=white" height="50" width="300"></td>
-            <td><img src="https://img.shields.io/badge/Playstation%205-003791?style=for-the-badge&logo=playstation-5&logoColor=white" height="50" width="300"></td>
-            <td><img src="https://img.shields.io/badge/Wii-8B8B8B?style=for-the-badge&logo=wii&logoColor=white" height="50" width="300"></td>
-        </tr>      
-        <tr>
-            <td><img src="https://img.shields.io/badge/Switch-E60012?style=for-the-badge&logo=nintendo-switch&logoColor=white" height="50" width="300"></td>
-            <td><img src="https://img.shields.io/badge/xbox-%23107C10.svg?style=for-the-badge&logo=xbox&logoColor=white" height="50" width="300"></td>
-        </tr>
-                            
-    </table>
-    
-    
-
+    <div class="divConhecimento">
+        <div class="divConhecimentoCelula">
+            <img src="https://img.shields.io/badge/Playstation%202-003791?style=for-the-badge&logo=playstation-2&logoColor=white" height="50" width="300">
+        </div>
+        <div class="divConhecimentoCelula">
+            <img src="https://img.shields.io/badge/Playstation%203-003791?style=for-the-badge&logo=playstation-3&logoColor=white" height="50" width="300">
+        </div>
+        <div class="divConhecimentoCelula">
+            <img src="https://img.shields.io/badge/Playstation%204-003791?style=for-the-badge&logo=playstation-4&logoColor=white" height="50" width="300">
+        </div>
+        <div class="divConhecimentoCelula">
+            <img src="https://img.shields.io/badge/Playstation%205-003791?style=for-the-badge&logo=playstation-5&logoColor=white" height="50" width="300">
+        </div>
+        <div class="divConhecimentoCelula">
+            <img src="https://img.shields.io/badge/Wii-8B8B8B?style=for-the-badge&logo=wii&logoColor=white" height="50" width="300">
+        </div>
+        <div class="divConhecimentoCelula">
+            <img src="https://img.shields.io/badge/Switch-E60012?style=for-the-badge&logo=nintendo-switch&logoColor=white" height="50" width="300">
+        </div>
+        <div class="divConhecimentoCelula">
+            <img src="https://img.shields.io/badge/xbox-%23107C10.svg?style=for-the-badge&logo=xbox&logoColor=white" height="50" width="300">
+        </div>        
+    </div>    
 </template>
